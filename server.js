5c5260eba588605f9644f7347f62dd096d9f11a6
Cbear1234 <amywonderland77@gmail.com>
2021-10-21 18:07:53 +0900

    sslport

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ try {
     cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/mima.miraclemind.kro.kr/cert.pem'), 'utf8').toString(),
   };
 
-  HTTPS.createServer(option, app).listen(sslport, () => {
-    colorConsole.success(`[HTTPS] Soda Server is started on port ${colors.cyan(sslport)}`);
+  HTTPS.createServer(option, app).listen(443, () => {
+    console.success(`[HTTPS] Soda Server is started on port ${443}`);
   });
 } catch (error) {
-  colorConsole.error('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
-  colorConsole.warn(error);
+  console.error('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
+  console.warn(error);
 }
 
 
