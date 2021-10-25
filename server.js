9920bb845da0634e8c7c9f19c6945af8883d416f
Cbear1234 <amywonderland77@gmail.com>
2021-10-25 16:36:18 +0900

    dbdb

--- server.js
+++ server.js
@@ -125,14 +125,14 @@ app.get('/:room', (req, res) => {
                 console.log("result : ", result);
                 console.log("_____방아이디 인서트 완료______");
 
-                conn.close(function (err) {
-                  console.log("db disconnected");
-                  if (err) {
-                    console.error('connection ended due to the error', err.message);
-                  }
-                });
-          });
-
+      });
+              
+      conn.close(function (err) {
+        console.log("db disconnected");
+        if (err) {
+          console.error('connection ended due to the error', err.message);
+        }
+      });
       
         
         res.render('room', { 
