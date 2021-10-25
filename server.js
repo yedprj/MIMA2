dbbbab810336667b308439dff3e48cf657bcc4df
Cbear1234 <amywonderland77@gmail.com>
2021-10-25 12:01:16 +0900

    cors

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.use(cors({origin: '*'}));
 //app.use('/peerjs', peerServer);
 
 app.all(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://mima.miraclemind.kro.kr:8080/");
   res.header("Access-Control-Allow-Origin", "https://mima.miraclemind.kro.kr:8443/");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
